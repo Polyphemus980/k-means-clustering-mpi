3835a4134ae7cad202d295dd0e2d0ecd480dd3b5
kTrzcinskii <kt.trzcinski@gmail.com>
2024-11-20 21:22:20 +0000

    Change data to store array of floats + create `Cluster` class

---
+++ src/k_means_data.cuh
@@ -0,0 +1,103 @@
+#ifndef K_MEANS_DATA
+#define K_MEANS_DATA
+
+#include <array>
+#include <vector>
+#include <filesystem>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <exception>
+
+namespace KMeansData
+{
+
+    template <size_t DIM>
+    class Cluster
+    {
+    private:
+        std::array<float, DIM> _coords;
+
+    public:
+        Cluster<DIM>()
+        {
+            this->_coords.fill(0.0);
+        }
+
+        Cluster<DIM>(const std::array<float, DIM> &coords)
+        {
+            this->_coords = coords;
+        }
+    };
+
+    template <size_t DIM>
+    class KMeansData
+    {
+    private:
+        size_t _points_count;
+        size_t _clusters_count;
+        // This is vector of all coords combinec
+        // For example for DIM = 3 and 2 points its:
+        // [x1 x2 y1 y2 z1 z2]
+        std::vector<float> _values;
+
+    public:
+        KMeansData<DIM>(const std::filesystem::path &filePath)
+        {
+            std::ifstream file{filePath};
+
+            if (!file)
+            {
+                throw std::runtime_error{"Error while reading input from file: file not opened"};
+            }
+
+            std::string line;
+            // load first line with N and k
+            if (!std::getline(file, line))
+            {
+                throw std::runtime_error("Error input file: empty file");
+            }
+            std::istringstream iss{line};
+            if (!(iss >> this->_points_count >> this->_clusters_count))
+            {
+                throw std::runtime_error("Invalid first line, expected \"<POINTS_COUNT> <CLUSTERS_COUNT>\"");
+            }
+
+            this->_values.resize(this->_points_count * DIM);
+            for (size_t i = 0; i < this->_points_count; i++)
+            {
+                if (!std::getline(file, line))
+                {
+                    throw std::runtime_error{"Invalid input file: too few points provided"};
+                }
+                std::istringstream iss{line};
+                for (size_t j = 0; j < DIM; j++)
+                {
+                    iss >> this->_values[j * this->_points_count + i];
+                }
+            }
+        }
+
+        const std::vector<float> &getValues() const
+        {
+            return this->_values;
+        }
+
+        const float *getRawValues() const
+        {
+            return this->_values.data();
+        }
+
+        static float __inline_hint__ __host__ getPointCoord(const KMeansData<DIM> &data, size_t pointIndex, size_t coordIndex)
+        {
+            return data._values[coordIndex * data._points_count + pointIndex];
+        }
+
+        static float __inline_hint__ __device__ getPointCoord(float *data, size_t pointsCount, size_t pointIndex, size_t coordIndex)
+        {
+            return data[coordIndex * pointsCount + pointIndex];
+        }
+    };
+} // KMeansData
+
+#endif // K_MEANS_DATA
--- src/main.cu
+++ src/main.cu
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <cuda_runtime.h>
 
-#include "k_means_data_cpu.cuh"
+#include "k_means_data.cuh"
 
 int main(int argc, char **argv)
 {
-    constexpr size_t DIM = 2;
+    constexpr size_t DIM = 3;
     std::string inputPath = argc == 1 ? "input/test_input.txt" : argv[1];
-    KMeansDataCPU::KMeansData<DIM> h_kMeansData{inputPath};
+    KMeansData::KMeansData<DIM> h_kMeansData{inputPath};
+    auto firstX = KMeansData::KMeansData<DIM>::getPointCoord(h_kMeansData, 0, 0);
+    std::cout << "X of first point: " << firstX << "\n";
+    auto secondZ = KMeansData::KMeansData<DIM>::getPointCoord(h_kMeansData, 1, 2);
+    std::cout << "Z of second point: " << secondZ << "\n";
     return 0;
 }

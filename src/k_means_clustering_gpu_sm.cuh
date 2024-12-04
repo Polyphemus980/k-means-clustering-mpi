f23055c1a072215834d2f72f0e1fe1b636b075b5
kTrzcinskii <kt.trzcinski@gmail.com>
2024-12-04 12:14:15 +0000

    Properly deallocate data when error is thrown

--- src/k_means_clustering_gpu_sm.cuh
+++ src/k_means_clustering_gpu_sm.cuh
@@ -168,93 +168,137 @@ namespace KMeansClusteringGPUSM
             throw std::runtime_error("Required shared memory exceeds device limits");
         }
 
-        // GPU allocations
-        size_t *d_memberships;
-        CHECK_CUDA(cudaMalloc(&d_memberships, sizeof(size_t) * d_data.pointsCount));
-        // We initialize the array that membership[i] = size_t::MAX
-        CHECK_CUDA(cudaMemset(d_memberships, 0xFF, sizeof(size_t) * d_data.pointsCount));
-
-        size_t *d_clustersMembershipCount;
-        CHECK_CUDA(cudaMalloc(&d_clustersMembershipCount, sizeof(size_t) * d_data.clustersCount));
-
-        float *d_newClusters;
-        // We have separate clustersValues for each block
-        CHECK_CUDA(cudaMalloc(&d_newClusters, sizeof(float) * d_data.clustersCount * DIM * newClustersBlocksCount));
-
-        uint32_t *d_newClustersMembershipCount;
-        // We have separate clustersCount for each block
-        CHECK_CUDA(cudaMalloc(&d_newClustersMembershipCount, sizeof(uint32_t) * d_data.clustersCount * newClustersBlocksCount));
-
-        int *d_shouldContinue;
-        CHECK_CUDA(cudaMalloc(&d_shouldContinue, sizeof(int) * newClustersBlocksCount));
-
-        // CPU allocation
+        size_t *d_memberships = nullptr;
+        size_t *d_clustersMembershipCount = nullptr;
+        float *d_newClusters = nullptr;
+        uint32_t *d_newClustersMembershipCount = nullptr;
+        int *d_shouldContinue = nullptr;
         int *shouldContinue = (int *)malloc(sizeof(int) * newClustersBlocksCount);
-        if (shouldContinue == nullptr)
-        {
-            throw std::runtime_error("Cannot allocate memory");
-        }
-
-        printf("[START] K-means clustering (main algorithm)\n");
-        gpuTimer.start();
-        // We don't need to call cudaDeviceSynchronzie because we use single device and we don't use cuda streams
-        for (size_t k = 0; k < Consts::MAX_ITERATION; k++)
-        {
-            // Calculate new membership
-            calculateMembershipAndNewClusters<DIM><<<newClustersBlocksCount, Consts::THREADS_PER_BLOCK, newClustersSharedMemorySize>>>(d_data, d_newClusters, d_newClustersMembershipCount, d_memberships, d_shouldContinue);
-            CHECK_CUDA(cudaGetLastError());
-            CHECK_CUDA(cudaDeviceSynchronize());
-
-            // If all blocks return false than we know that no change was made and we can break from loop
-            CHECK_CUDA(cudaMemcpy(shouldContinue, d_shouldContinue, sizeof(int) * newClustersBlocksCount, cudaMemcpyDeviceToHost));
-            size_t totalShouldContinue = 0;
-            for (size_t b = 0; b < newClustersBlocksCount; b++)
-            {
-                totalShouldContinue += shouldContinue[b];
-            }
-            printf("[INFO] Iteration: %ld, changed points: %ld\n", k, totalShouldContinue);
-            if (totalShouldContinue == 0)
-            {
-                break;
-            }
-
-            // Accumulate counts from all blocks from previous kernel
-            accumulateNewClustersMemerships<<<accumulateNewClustersMemershipsBlocksCount, d_data.clustersCount>>>(d_data, d_clustersMembershipCount, d_newClustersMembershipCount, newClustersBlocksCount);
-            CHECK_CUDA(cudaGetLastError());
-
-            // Calculate new clusters
-            updateClusters<DIM><<<updateClustersBlocksCount, d_data.clustersCount * DIM>>>(d_data, d_clustersMembershipCount, d_newClusters, newClustersBlocksCount);
-            CHECK_CUDA(cudaGetLastError());
-        }
-        gpuTimer.end();
-        gpuTimer.printResult("K-means clustering (main algorithm)");
 
         // Prepare memory for storing results on CPU side
         thrust::host_vector<float> clustersValues(d_data.clustersCount * DIM);
         thrust::host_vector<size_t> membership(d_data.pointsCount);
 
-        // Wait for GPU to finish calculations
-        CHECK_CUDA(cudaDeviceSynchronize());
+        bool isError = false;
+        std::runtime_error error("placeholder");
 
-        // Copy result from GPU to CPU
-        printf("[START] Copy data from GPU to CPU\n");
-        cpuTimer.start();
-        CHECK_CUDA(cudaMemcpy(clustersValues.data(), d_data.d_clustersValues, sizeof(float) * clustersValues.size(), cudaMemcpyDeviceToHost));
-        CHECK_CUDA(cudaMemcpy(membership.data(), d_memberships, sizeof(size_t) * d_data.pointsCount, cudaMemcpyDeviceToHost));
-        cpuTimer.end();
-        cpuTimer.printResult("Copy data from GPU to CPU");
+        try
+        {
+            // GPU allocations
+            CHECK_CUDA(cudaMalloc(&d_memberships, sizeof(size_t) * d_data.pointsCount));
+            // We initialize the array that membership[i] = size_t::MAX
+            CHECK_CUDA(cudaMemset(d_memberships, 0xFF, sizeof(size_t) * d_data.pointsCount));
 
+            CHECK_CUDA(cudaMalloc(&d_clustersMembershipCount, sizeof(size_t) * d_data.clustersCount));
+
+            // We have separate clustersValues for each block
+            CHECK_CUDA(cudaMalloc(&d_newClusters, sizeof(float) * d_data.clustersCount * DIM * newClustersBlocksCount));
+
+            // We have separate clustersCount for each block
+            CHECK_CUDA(cudaMalloc(&d_newClustersMembershipCount, sizeof(uint32_t) * d_data.clustersCount * newClustersBlocksCount));
+
+            CHECK_CUDA(cudaMalloc(&d_shouldContinue, sizeof(int) * newClustersBlocksCount));
+
+            // CPU allocation
+            if (shouldContinue == nullptr)
+            {
+                throw std::runtime_error("Cannot allocate memory");
+            }
+
+            printf("[START] K-means clustering (main algorithm)\n");
+            gpuTimer.start();
+            // We don't need to call cudaDeviceSynchronzie because we use single device and we don't use cuda streams
+            for (size_t k = 0; k < Consts::MAX_ITERATION; k++)
+            {
+                // Calculate new membership
+                calculateMembershipAndNewClusters<DIM><<<newClustersBlocksCount, Consts::THREADS_PER_BLOCK, newClustersSharedMemorySize>>>(d_data, d_newClusters, d_newClustersMembershipCount, d_memberships, d_shouldContinue);
+                CHECK_CUDA(cudaGetLastError());
+                CHECK_CUDA(cudaDeviceSynchronize());
+
+                // If all blocks return false than we know that no change was made and we can break from loop
+                CHECK_CUDA(cudaMemcpy(shouldContinue, d_shouldContinue, sizeof(int) * newClustersBlocksCount, cudaMemcpyDeviceToHost));
+                size_t totalShouldContinue = 0;
+                for (size_t b = 0; b < newClustersBlocksCount; b++)
+                {
+                    totalShouldContinue += shouldContinue[b];
+                }
+                printf("[INFO] Iteration: %ld, changed points: %ld\n", k, totalShouldContinue);
+                if (totalShouldContinue == 0)
+                {
+                    break;
+                }
+
+                // Accumulate counts from all blocks from previous kernel
+                accumulateNewClustersMemerships<<<accumulateNewClustersMemershipsBlocksCount, d_data.clustersCount>>>(d_data, d_clustersMembershipCount, d_newClustersMembershipCount, newClustersBlocksCount);
+                CHECK_CUDA(cudaGetLastError());
+
+                // Calculate new clusters
+                updateClusters<DIM><<<updateClustersBlocksCount, d_data.clustersCount * DIM>>>(d_data, d_clustersMembershipCount, d_newClusters, newClustersBlocksCount);
+                CHECK_CUDA(cudaGetLastError());
+            }
+            gpuTimer.end();
+            gpuTimer.printResult("K-means clustering (main algorithm)");
+
+            // Wait for GPU to finish calculations
+            CHECK_CUDA(cudaDeviceSynchronize());
+
+            // Copy result from GPU to CPU
+            printf("[START] Copy data from GPU to CPU\n");
+            cpuTimer.start();
+            CHECK_CUDA(cudaMemcpy(clustersValues.data(), d_data.d_clustersValues, sizeof(float) * clustersValues.size(), cudaMemcpyDeviceToHost));
+            CHECK_CUDA(cudaMemcpy(membership.data(), d_memberships, sizeof(size_t) * d_data.pointsCount, cudaMemcpyDeviceToHost));
+            cpuTimer.end();
+            cpuTimer.printResult("Copy data from GPU to CPU");
+        }
+        catch (const std::runtime_error &e)
+        {
+            fprintf(stderr, "[ERROR]: %s", e.what());
+            isError = true;
+            error = e;
+            goto ERROR_HANDLING;
+        }
+
+    ERROR_HANDLING:
         // GPU deallocations
-        cudaFree(d_memberships);
-        cudaFree(d_clustersMembershipCount);
-        cudaFree(d_newClusters);
-        cudaFree(d_newClustersMembershipCount);
-        cudaFree(d_shouldContinue);
-        cudaFree(d_data.d_pointsValues);
-        cudaFree(d_data.d_clustersValues);
+        if (d_memberships != nullptr)
+        {
+            cudaFree(d_memberships);
+        }
+        if (d_clustersMembershipCount != nullptr)
+        {
+            cudaFree(d_clustersMembershipCount);
+        }
+        if (d_newClusters != nullptr)
+        {
+            cudaFree(d_newClusters);
+        }
+        if (d_newClustersMembershipCount != nullptr)
+        {
+            cudaFree(d_newClustersMembershipCount);
+        }
+        if (d_shouldContinue != nullptr)
+        {
+            cudaFree(d_shouldContinue);
+        }
+        if (d_data.d_pointsValues != nullptr)
+        {
+            cudaFree(d_data.d_pointsValues);
+        }
+        if (d_data.d_clustersValues != nullptr)
+        {
+            cudaFree(d_data.d_clustersValues);
+        }
 
         // CPU deallocation
-        free(shouldContinue);
+        if (shouldContinue != nullptr)
+        {
+            free(shouldContinue);
+        }
+
+        if (isError)
+        {
+            throw error;
+        }
 
         return Utils::ClusteringResult{
             .clustersValues = clustersValues,
--- src/k_means_data.cuh
+++ src/k_means_data.cuh
@@ -97,19 +97,37 @@ namespace KMeansData
         KMeansDataGPU transformToGPURepresentation() const
         {
             CpuTimer::Timer cpuTimer;
-            float *d_pointsValues;
-            float *d_clustersValues;
-            CHECK_CUDA(cudaMalloc(&d_pointsValues, sizeof(float) * _values.size()));
-            CHECK_CUDA(cudaMalloc(&d_clustersValues, sizeof(float) * _clustersValues.size()));
+            float *d_pointsValues = nullptr;
+            float *d_clustersValues = nullptr;
 
-            printf("[START] Copy data from CPU to GPU\n");
-            cpuTimer.start();
+            try
+            {
+                CHECK_CUDA(cudaMalloc(&d_pointsValues, sizeof(float) * _values.size()));
+                CHECK_CUDA(cudaMalloc(&d_clustersValues, sizeof(float) * _clustersValues.size()));
 
-            CHECK_CUDA(cudaMemcpy(d_pointsValues, _values.data(), sizeof(float) * _values.size(), cudaMemcpyHostToDevice));
-            CHECK_CUDA(cudaMemcpy(d_clustersValues, _clustersValues.data(), sizeof(float) * _clustersValues.size(), cudaMemcpyHostToDevice));
+                printf("[START] Copy data from CPU to GPU\n");
+                cpuTimer.start();
 
-            cpuTimer.end();
-            cpuTimer.printResult("Copy data from CPU to GPU");
+                CHECK_CUDA(cudaMemcpy(d_pointsValues, _values.data(), sizeof(float) * _values.size(), cudaMemcpyHostToDevice));
+                CHECK_CUDA(cudaMemcpy(d_clustersValues, _clustersValues.data(), sizeof(float) * _clustersValues.size(), cudaMemcpyHostToDevice));
+
+                cpuTimer.end();
+                cpuTimer.printResult("Copy data from CPU to GPU");
+            }
+            catch (const std::runtime_error &e)
+            {
+                fprintf(stderr, "[ERROR]: %s", e.what());
+                // We only want to deallocate in case of error - otherwise it will be deallocated by the caller of this function
+                if (d_pointsValues != nullptr)
+                {
+                    cudaFree(d_pointsValues);
+                }
+                if (d_clustersValues != nullptr)
+                {
+                    cudaFree(d_clustersValues);
+                }
+                throw e;
+            }
 
             return KMeansDataGPU{
                 .pointsCount = _pointsCount,
